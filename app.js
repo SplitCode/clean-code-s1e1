dfaae00a7aeb4e1175349a7fa4392b47f6a043e0
SplitCode <natacsp@mail.ru>
2024-01-08 18:55:20 +0300

    refactor: add alternative content for media according rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="button delete-button";
   deleteButtonImg.src="./remove.svg";
   deleteButtonImg.className="img";
+  deleteButtonImg.alt="delete button";
   deleteButton.appendChild(deleteButtonImg);
 
 
